wilhelm-murdoch/gost

commit eb45e506d230e7a0f0f07228edf4206bf9819170

gost: Simple clean up and correction of spelling.

gost.go
@@ -3,7 +3,7 @@ package main
 import (
 	"code.google.com/p/goauth2/oauth"
 	"fmt"
-	goopt "github.com/droundy/goopt"
+	"github.com/droundy/goopt"
 	"github.com/google/go-github/github"
 	"io/ioutil"
 	"os"
@@ -29,23 +29,16 @@ func main() {
 	goopt.Parse(nil)
 
 	if len(strings.TrimSpace(*file)) == 0 {
-		fmt.Print("Please specify a valid file with -f or --file")
-		return
-	}
-
-	if _, err := os.Stat(*file); os.IsNotExist(err) {
-		fmt.Printf("No such file: %s", *file)
+		fmt.Fprintln(os.Stderr, "Please specify a valid file with -f or --file")
 		return
 	}
 
 	bytes, err := ioutil.ReadFile(*file)
-
-	content := string(bytes)
-
 	if err != nil {
-		fmt.Printf("Invalid file specified: %s", *file)
+		fmt.Fprintf(os.Stderr, "Invalid file specified; %v\n", err)
 		return
 	}
+	content := string(bytes)
 
 	if len(strings.TrimSpace(*name)) == 0 {
 		*name = path.Base(*file)
@@ -64,22 +57,21 @@ func main() {
 		client = github.NewClient(t.Client())
 	}
 
-	input := new(github.Gist)
-	input.Description = description
-	input.Public = public
-	input.Files = map[github.GistFilename]github.GistFile{
-		github.GistFilename(*name): github.GistFile{Content: &content},
+	input := &github.Gist{
+		Description: description,
+		Public:      public,
+		Files: map[github.GistFilename]github.GistFile{
+			github.GistFilename(*name): github.GistFile{Content: &content},
+		},
 	}
 
 	fmt.Print("Gosting Gist ... ")
 
 	gist, _, err := client.Gists.Create(input)
-
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Unable to create gist: %v", err)
 	}
 
-	fmt.Print("Done!")
-	fmt.Println("")
-	fmt.Printf("Gist URL: %s", string(*gist.HTMLURL))
+	fmt.Println("Done!")
+	fmt.Println("Gist URL:", string(*gist.HTMLURL))
 }

commit d378e29f979f6c157b702376bc69afce4335cf5d

gost: Fix bug introduced in rev eb45e50.

gost.go
@@ -70,6 +70,7 @@ func main() {
 	gist, _, err := client.Gists.Create(input)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create gist: %v", err)
+		return
 	}
 
 	fmt.Println("Done!")